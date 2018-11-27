ce6a32cd930895c4790f07de176ce2af8bd6ea7f
ayang123 <alex.yangdeqian@gmail.com>
2018-11-26 19:32:07 -0500

    Update create.sql

--- create.sql
+++ create.sql
@@ -70,3 +70,10 @@ CREATE TABLE Has (
     proficiency    INTEGER,
     PRIMARY KEY    (candidateEmail, skillId)
 );
+
+DROP TABLE IF EXISTS jobs.Has;
+DROP TABLE IF EXISTS jobs.Apply;
+DROP TABLE IF EXISTS jobs.Skill;
+DROP TABLE IF EXISTS jobs.Candidate;
+DROP TABLE IF EXISTS jobs.Job;
+DROP TABLE IF EXISTS jobs.Organization;
